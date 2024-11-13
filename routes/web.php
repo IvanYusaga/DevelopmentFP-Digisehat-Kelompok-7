55c51930a1734fcecd84c388a88c477b68364cc7
AdjieArrayan <arrayan54181@gmail.com>
2024-11-13 22:54:01 +0700

    menambahkan route pada web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,67 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Buat Admin
+
+Route::get('/admin/wireframe', function () {
+    return view('admin.wireframe');
+});
+
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard');
+});
+
+Route::get('/admin/management', function () {
+    return view('admin.management');
+});
+
+Route::get('/admin/profile', function () {
+    return view('admin.profile');
+});
+
+
+
+//Buat User
+Route::get('/user/userDashboard', function () {
+    return view('user.userDashboard');
+});
+
+Route::get('/user/userProfile', function () {
+    return view('user.userProfile');
+});
+
+    //Manajemen Obat User
+Route::get('/user/userManagement', function () {
+    return view('user.userManagement');
+});
+
+    Route::get('/user/formulirObat', function () {
+        return view('user.manajemenObat.formulirObat');
+    });
+
+    Route::get('/user/informasiObat', function () {
+        return view('user.manajemenObat.informasiObat');
+    });
+
+    //Atur Jadwal User
+Route::get('/user/userJadwal', function () {
+    return view('user.userJadwal');
+});
+
+    Route::get('/user/formulirJadwal', function () {
+        return view('user.aturJadwal.formulirJadwal');
+    });
+
+Route::get('/user/userRiwayat', function () {
+    return view('user.userRiwayat');
+});
+
+Route::get('/user/userLogbook', function () {
+    return view('user.userLogbook');
+});
+
+Route::get('/user/userBMI', function () {
+    return view('user.userBMI');
+});
+
