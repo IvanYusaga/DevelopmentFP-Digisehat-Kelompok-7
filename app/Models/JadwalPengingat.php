d5a977f9b0c9aab317521ef0079c69eb498cd390
AlyaaWS <alyawahyuning93@gmail.com>
2024-11-14 03:01:30 +0700

    mengubah konfigurasi database mysql

---
+++ app/Models/JadwalPengingat.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class JadwalPengingat extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'id_jadwal';
+
+    protected $fillable = [
+        'id_obat',
+        'durasi_pengingat',
+        'dosis',
+        'jumlah_obat',
+        'frekuensi',
+        'tanggal_konsumsi',
+        'status',
+    ];
+
+    public function obat(): BelongsTo
+    {
+        return $this->belongsTo(Obat::class, 'id_obat');
+    }
+}
