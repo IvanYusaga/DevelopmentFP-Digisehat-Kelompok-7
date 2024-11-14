e300388a8677cae2bf25fa405fcd8b447cd49dc8
Ivan Yusaga <yusagaivan@gmail.com>
2024-11-14 17:03:26 +0700

    update ObatController (Menambahkan Delete, Menampilkan Edit Obat, Logika Edit Obat)

--- app/Http/Controllers/ObatController.php
+++ app/Http/Controllers/ObatController.php
@@ -7,16 +7,15 @@ use Illuminate\Http\Request;
 
 class ObatController extends Controller
 {
+    // Tampilan View Obat
     public function index()
     {
-        // Menampilkan form
         return view('user.manajemenObat.formulirObat');
     }
 
+    // Menambahkan Data Obat
     public function postObat(Request $request)
     {
-
-        // Validasi data
         $request->validate([
             'nama_obat' => 'required|string|max:255',
             'date' => 'required|date',
@@ -24,7 +23,6 @@ class ObatController extends Controller
             'deskripsi' => 'required|string',
         ]);
 
-        // Menyimpan data ke dalam tabel `obats`
         Obat::create([
             'nama_obat' => $request->nama_obat,
             'date' => $request->date,
@@ -32,9 +30,54 @@ class ObatController extends Controller
             'deskripsi' => $request->deskripsi,
         ]);
 
-
-
-        // Redirect ke halaman lain setelah berhasil submit
         return redirect()->route('informasiObat')->with('success', 'Data obat berhasil disimpan');
+    }
+
+    // Menampilkan Data Obat
+    public function informasiObat()
+    {
+        $obats = Obat::all();
+
+        return view('user.manajemenObat.informasiObat', compact('obats'));
+    }
+
+    // Menampilkan Edit Obat
+    public function editObat($id)
+    {
+        $obat = Obat::findOrFail($id);
+
+        return view('user.manajemenObat.editObat', compact('obat'));
+    }
+
+    // Update Data Obat
+    public function updateObat(Request $request, $id)
+    {
+        // Validasi data
+        $request->validate([
+            'nama_obat' => 'required|string|max:255',
+            'date' => 'required|date',
+            'penggunaan_obat' => 'required|string|max:255',
+            'deskripsi' => 'required|string',
+        ]);
+
+        $obat = Obat::findOrFail($id);
+        $obat->update([
+            'nama_obat' => $request->nama_obat,
+            'date' => $request->date,
+            'penggunaan_obat' => $request->penggunaan_obat,
+            'deskripsi' => $request->deskripsi,
+        ]);
+
+        return redirect()->route('informasiObat')->with('success', 'Data obat berhasil diperbarui');
+    }
+
+
+    // Delete Data Obat
+    public function destroy($id)
+    {
+        $obat = Obat::findOrFail($id);
+        $obat->delete();
+
+        return redirect()->route('informasiObat')->with('success', 'Data obat berhasil dihapus');
     }
 }
