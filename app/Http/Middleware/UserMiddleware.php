461fdcd699f22a32e4093c553b15cb3f833e33ba
Nama Anda <emailanda@example.com>
2024-11-19 18:23:49 +0700

    membuat middleware baru admin dan user

---
+++ app/Http/Middleware/UserMiddleware.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        // Cek apakah user sudah login dan memiliki role user
+        if (Auth::check() && Auth::user()->role === 'user') {
+            return $next($request);
+        }
+        return redirect('/login')->with('error', 'Access denied.');
+    }
+}
