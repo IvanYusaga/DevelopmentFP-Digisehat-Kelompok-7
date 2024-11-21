263b2f6aaa995defedc0fbe9152197e240b3893c
AlyaaWS <alyawahyuning93@gmail.com>
2024-11-21 11:39:12 +0700

    modified:   app/Http/Controllers/ObatController.php, modified:   app/Models/Obat.php, modified:   resources/views/user/aturJadwal/formulirJadwal.blade.ph,  modified:   routes/web.php

--- resources/views/user/aturJadwal/formulirJadwal.blade.php
+++ resources/views/user/aturJadwal/formulirJadwal.blade.php
@@ -13,37 +13,41 @@
       <div class="card-body">
         <h5 class="card-title text-center">Atur Jadwal Obat</h5>
 
-        <form class="row g-3" id="scheduleForm">
+        <form class="row g-3" id="scheduleForm" action="{{ route('jadwalPengingat.store') }}" method="POST">
+            @csrf
           <div class="col-12">
             <label for="inputNamaObat" class="form-label text-primary">Nama Obat</label>
-            <input type="text" class="form-control" id="inputNamaObat" placeholder="">
+            <select class="form-control" id="inputNamaObat" name="id_obat">
+                <option value="">Pilih Obat</option>
+                @foreach($obats as $obat)
+                    <option value="{{ $obat->id_obat }}">{{ $obat->nama_obat }}</option>
+                @endforeach
+            </select>
           </div>
           <div class="col-12">
             <label for="inputDosis" class="form-label text-primary">Dosis</label>
-            <input type="text" class="form-control" id="inputDosis" placeholder="">
+            <input type="text" class="form-control" id="inputDosis" name="dosis" placeholder="">
           </div>
           <div class="col-12">
             <label for="inputJumlahObat" class="form-label text-primary">Jumlah Obat</label>
-            <input type="text" class="form-control" id="inputJumlahObat" placeholder="">
+            <input type="text" class="form-control" id="inputJumlahObat" name="jumlah_obat" placeholder="">
           </div>
           <div class="col-12">
             <label for="inputFrekuensi" class="form-label text-primary">Frekuensi (Berapa kali minum obat)</label>
-            <input type="text" class="form-control" id="inputFrekuensi" placeholder="">
+            <input type="text" class="form-control" id="inputFrekuensi" name="frekuensi" placeholder="">
           </div>
           <div class="col-12">
             <label for="inputDurasiPengingat" class="form-label text-primary">Durasi Pengingat (Hari)</label>
-            <input type="text" class="form-control" id="inputDurasiPengingat" placeholder="">
+            <input type="text" class="form-control" id="inputDurasiPengingat" name="durasi_pengingat" placeholder="">
           </div>
           <div class="col-8">
             <label for="inputDate" class="col-sm-4 col-form-label text-primary">Jadwal Pengingat</label>
             <div class="col-sm-10">
-              <input type="date" class="form-control text-primary" id="inputDate">
+              <input type="date" class="form-control text-primary" id="inputDate" name="tanggal_konsumsi">
             </div>
           </div>
         <div class="text-center mt-3">
-            <a>
-                <button type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-info rounded-pill text-white" id="basic-info-trigger">Buka Pengingat di Google Calendar</button>
-            </a>
+            <button type="submit" class="btn btn-info rounded-pill text-white">Simpan Jadwal Pengingat</button>
         </div>
         </form>
       </div>
