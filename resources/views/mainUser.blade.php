e5bdb71e412ab005f36fbefc0031fbadcd92a24b
Ivan Yusaga <yusagaivan@gmail.com>
2024-11-22 22:20:14 +0700

    memperbarui routes pada mainUser

--- resources/views/mainUser.blade.php
+++ resources/views/mainUser.blade.php
@@ -38,7 +38,7 @@
   <header id="header" class="header fixed-top d-flex align-items-center">
 
     <div class="d-flex align-items-center justify-content-between">
-      <a href="index.html" class="logo d-flex align-items-center">
+      <a href="{{ route('userDashboard') }}" class="logo d-flex align-items-center">
         <img src="{{asset('style/assets/img/logo.jpg')}}" alt="">
         <span class="d-none d-lg-block text-primary">MediPulse</span>
       </a>
